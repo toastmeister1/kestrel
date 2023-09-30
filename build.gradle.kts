66b5f06b8ee2b46fd9401ea6bed733afa6c7decf
YeongSang Jeon <wjsdudtkd95@gmail.com>
2023-09-30 15:40:44 +0900

    깃 훅 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,3 +21,28 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlinAndroid) apply false
 }
+
+tasks {
+    register<Copy>("copyGitHooks") {
+        description = "Copies the git hooks from /git-hooks to the .git folder."
+        group = "git hooks"
+        from("$rootDir/scripts/pre-commit")
+        into("$rootDir/.git/hooks/")
+    }
+
+    register<Exec>("installGitHooks") {
+        description = "Installs the pre-commit git hooks from /git-hooks."
+        group = "git hooks"
+        workingDir = rootDir
+        commandLine = listOf("chmod")
+        args("-R", "+x", ".git/hooks/")
+        dependsOn("copyGitHooks")
+        doLast {
+            logger.info("Git hook installed successfully.")
+        }
+    }
+
+    afterEvaluate {
+        tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
+    }
+}
--- lint/src/main/java/com/toastmeister1/kestrel/lint/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.toastmeister1.kestrel.lint
-
-class MyClass {
-}
